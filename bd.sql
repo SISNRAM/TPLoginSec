0a870bb6120bdf08eba63aa9eda375ab1976f2e1
N_Ramzi <64749241+SISNRAM@users.noreply.github.com>
2023-02-06 22:51:33 +0100

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -6,6 +6,6 @@ USE loginsystem;
 
 -- Create table
 CREATE TABLE login (
-  id VARCHAR(50) NOT NULL,
-  password VARCHAR(50) NOT NULL
+  id VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
